8f611e75526e492c7134d65e9a39fd2a368a47ca
Colin <colinreidlapierre@gmail.com>
2017-10-08 22:18:57 -0400

    Suppression du constructeur ( On utilise StartConnection() directement pour la connection.

--- TP-1/src/CommForme.java
+++ TP-1/src/CommForme.java
@@ -2,19 +2,14 @@
 public class CommForme extends Comm {
 	
 	public String forme;
-	Comm comm = new Comm();
-		
-	public CommForme(){
-		comm.startConnection();	
-	}
 	
 	public void envoieGET() {
-		comm.sendString("GET");
-		forme = comm.getString();
+		this.sendString("GET");
+		forme = this.getString();
 	}
 		
 	public void envoieEND() {
-	    comm.endConnection();
+	    this.endConnection();
 	}
 	
 }
