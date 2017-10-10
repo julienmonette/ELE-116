79917df1a1986111979b7a67a8c28344a75d714b
Colin <colinreidlapierre@gmail.com>
2017-10-10 12:20:15 -0400

    Ajout de la conversion.

--- TP-1/src/Cercle.java
+++ TP-1/src/Cercle.java
@@ -4,21 +4,42 @@ import java.awt.Color;
 public class Cercle extends Forme{
 	
 	private int x1;
-	private int x2;
 	private int y1;
-	private int y2;
+	private int r;
 	
+	private int x;
+	private int y;
+	private int h;
+	private int w;
 	
 	public Cercle(int x,int y, int r) {
 		x1 = x;    // TODO TODO TODO 
-		x2 = y;
-		y1 = r;
-		y2 = r;
+		y1 = y;
+		this.r = r;
+		convertir();
 	}
 	
 	void dessine(Graphics g) {
 		g.setColor(Color.GREEN);
-		g.fillOval(x1, x2, y1, y2);
+		g.fillOval(x, y, h, w);
 	}
+
+	public void convertir()
+	{
+		if(x1 > r)
+			x = x1-r;
+		else
+			x = x1;
+		
+		if(y1 > r)
+			y = y1-r;
+		else
+			y = y1;
+		
+		
+		w = 2*r;
+		h = 2*r;
+	}	
+	
 	
 }
