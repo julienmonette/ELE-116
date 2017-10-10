3a338a2176af9ef4b49ef7ab56d96e409d93d1c3
Colin <colinreidlapierre@gmail.com>
2017-10-09 20:03:04 -0400

    Modification à la class mère forme. ( fonctions que je ne comprennais pas ? )

--- TP-1/src/Forme.java
+++ TP-1/src/Forme.java
@@ -1,10 +1,16 @@
+import java.awt.Graphics;
 
 public abstract class Forme 
 {
-	abstract void SetCoordinate(int x, int y);
-	abstract void SetShape(int lenght, int height);
+//	abstract void SetCoordinate(int x1, int y);
+//	abstract void SetShape(int lenght, int height);
+//	
+//	
+    abstract void dessine(Graphics g);
+	
+	public Forme() {};
 	
 	
-	abstract void display();
+	
 	
 }
