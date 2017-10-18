8031a4c8e2200bf5c244ffa45938ddf824d1fdf1
Colin <colinreidlapierre@gmail.com>
2017-10-18 16:26:24 -0400

    Ajout de l'entête.

--- TP-1/src/TabFormes.java
+++ TP-1/src/TabFormes.java
@@ -1,3 +1,23 @@
+/******************************************************
+Cours : ELE116
+Session : AUT2017
+Groupe : 01
+Projet : Laboratoire #1
+Étudiant(e)(s) : 	Colin Reid-Lapierre
+					Julien Monette
+Code(s) perm. : 	REIC11069309
+					XXXXXXXXX					
+Professeur : Rita Noumeir
+Nom du fichier : TabFormes.java
+Date création : 2017-09-11
+Date dern. modif. 2017-10-18
+
+*******************************************************
+Historique des modifications
+*******************************************************
+2017-09-11 Version initiale
+*******************************************************/
+
 import java.awt.Graphics;
 import java.awt.geom.RectangularShape;
 import java.util.StringTokenizer;
@@ -33,15 +53,11 @@ public class TabFormes {
 		seq = Integer.parseInt(line.nextToken());
 
 		typeForme = line.nextToken();
-		System.out.println(seq);
-		System.out.println(typeForme);
 		
 		x1 = Integer.parseInt(line.nextToken());
 		x2 = Integer.parseInt(line.nextToken());
 		y1 = Integer.parseInt(line.nextToken());
-		if(!typeForme.equals("<CERCLE>")) {	y2 = Integer.parseInt(line.nextToken());}
-		
-		
+		if(!typeForme.equals("<CERCLE>")) {	y2 = Integer.parseInt(line.nextToken());}		
 		
 		if(typeForme.equals("<CARRE>")) {
 			forme = new Carre(x1,x2,y1,y2);
