3e3750efcbd7bf5018f61f09a0e19eb0a4f59efe
Colin <colinreidlapierre@gmail.com>
2017-11-01 15:02:37 -0400

    Construction de l'arbre

--- TP-2/src/arbre/Noeud.java
+++ TP-2/src/arbre/Noeud.java
@@ -1,5 +1,23 @@
 package arbre;
 
-public class Noeud {
+import java.util.Vector;
 
+public class Noeud{
+
+	private String data;
+
+	
+	
+	
+	public String getData() {
+		return this.data;	
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
+	
+	
 }
