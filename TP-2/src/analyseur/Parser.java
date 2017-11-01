ad37daabdd9eb0156d8f63940dc4a8f50311ea38
Colin <colinreidlapierre@gmail.com>
2017-10-31 21:44:30 -0400

    Ajout méthode ParseXMLFile (en conctruction)

--- TP-2/src/analyseur/Parser.java
+++ TP-2/src/analyseur/Parser.java
@@ -1,5 +1,38 @@
 package analyseur;
 
+import java.io.IOException;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 public class Parser {
 
+	XMLReader reader; 
+	
+	
+	public void parseXMLFile(String XLMFilename) {
+		try{
+			reader = XMLReaderFactory.createXMLReader();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		reader.setContentHandler(new MyContentHandler());
+		reader.setErrorHandler(new MyErrorHandler());
+		try {
+			reader.parse(XLMFilename);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
 }
