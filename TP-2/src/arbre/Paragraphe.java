9ca8c3802efae0cccfe770875c667a3231713bff
Colin <colinreidlapierre@gmail.com>
2017-11-02 21:52:56 -0400

    Modification mineures ne changeant pas le fonctionnement du programme

---
+++ TP-2/src/arbre/Paragraphe.java
@@ -0,0 +1,16 @@
+package arbre;
+
+import java.util.Vector;
+
+public class Paragraphe {
+
+	private String text="";
+	
+	public void setText(String s) {
+		this.text = s;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+}
