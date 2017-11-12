da247ececeb956e4c47388013dfed72dc68c15dc
Colin <colinreidlapierre@gmail.com>
2017-11-12 15:46:24 -0500

    Visiteur pour la création du fichier HTML qui sera affiché par la suite.

---
+++ TP-2/src/visitor/VisitorPrint.java
@@ -0,0 +1,34 @@
+package visitor;
+
+import arbre.Chapitre;
+import arbre.Livre;
+import arbre.Paragraphe;
+import arbre.Root;
+
+public class VisitorPrint implements Visitor {
+
+	@Override
+	public void visit(Root root) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(Chapitre chapitre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Livre livre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(Paragraphe Paragraphe) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
